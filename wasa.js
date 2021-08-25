10cea09a9e5b4cfa0c94c21d3113bb5c164b8a7a
Masami Nakada Bedrinana <72590217+MasamioNakada@users.noreply.github.com>
2021-08-25 00:56:30 -0500

    Update wasa.js

--- wasa.js
+++ wasa.js
@@ -5,7 +5,9 @@ const venom = require('venom-bot');
 
 
 venom
-  .create()
+  .create(
+    
+  )
   .then((client) => start(client))
   .catch((erro) => {
     console.log(erro);
@@ -14,34 +16,15 @@ venom
 
 async function start(client) {
   global.gclient = client
-  
-  global.stimmen = []
-  global.choices = []
-  global.amdownloaden = []
   global.sendingSticker = []
   global.sendingAnimatedSticker = []
-  global.queueSticker = []
-  global.queueAnimatedSticker = []
-  global.queuemp3 = []
-  global.queuemp4 = []
-  global.polllist = []
-  global.allSticker = []
+
   
   client.onMessage((message) => {
     client.sendSeen(message.from)
-    client.startTyping(message.from)
+    
       var onMessagee = require('./Scripts/onMessage')
     onMessagee.message(message)
-    
-    
-    
-
-
-      
-  })
-}
-
-
 
 
 
